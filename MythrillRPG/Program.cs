93b7a92a7d299a85e5be28eae079c6bb723a0168
Niek Melet <niek+github@meletstudios.com>
2023-11-27 19:00:06 +0100

    v0.0.2
    Started working on Room generation

--- MythrillRPG/Program.cs
+++ MythrillRPG/Program.cs
@@ -1,12 +1,20 @@
-﻿namespace MythrillRPG
+﻿using MythrillRPG.classes.room_generation;
+
+namespace MythrillRPG
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
             // Initialize a new game object and run the game
-            Game game = new Game();
-            game.RunGame();
+            //Game game = new Game();
+            //game.RunGame();
+
+            RoomGenerator generator = new RoomGenerator();
+            generator.InitializeRoom();
+            Console.SetWindowSize(101, 101);
+
+            Console.ReadKey();
         }
     }
 }
